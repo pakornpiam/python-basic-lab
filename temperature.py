6922f66449ced4828f30c03002422ae1e26d00e3
Pakorn <pakornpiam@gmail.com>
2022-12-09 21:42:47 +0700

    Add files via upload

---
+++ temperature.py
@@ -0,0 +1,17 @@
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+
+
+url = 'https://www.pttor.com/th/oil_price'
+webopen = urlopen(url) # open web with out web browzer
+html_page = webopen.read()
+#print(html_page)
+#webopen.close()
+#print(html_page)
+
+data = BeautifulSoup(html_page,'html.parser')# bs4 help to decode  to thai 
+print(data)
+temp = data.find('td',{'data-v-223e48cc'})
+#print (temp.text)
+
+
