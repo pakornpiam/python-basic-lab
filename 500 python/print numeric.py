76feec52f6ed902bf49bea65368ad45d0590b7bc
pakornpiam <pakornpiam@gmail.com>
2022-12-21 20:13:42 +0700

    done

---
+++ 500 python/print numeric.py
@@ -0,0 +1,51 @@
+print(25)
+print('%f' %100)
+from math import pi
+print (pi)
+
+
+print('%0.3f' %pi)
+
+
+a=2
+print(a)
+
+
+a = 12.5
+print(a)
+
+
+a = 2
+b = 3
+print(a, 'x' ,b ,'=', a*b )
+
+
+a = 2
+b = 3
+print(a,'+',b ,'=', b, '+', a , '=', a + b)
+
+a = 2 
+b = 3 
+c = 5
+
+print('%d * (%d+%d) = %d*%d + %d*%d'%(a,b,c,a,b,a,c))
+
+
+
+a = 2.4 
+b = 2.5
+
+print( a ,'+',b , '=' '%0.4f' %(a+b))
+
+a = 5
+b = 2 
+
+print('%0.2f- %0.2f = %0.2f'%(a,b,a-b))
+
+
+
+
+a = 2 
+b = 3.5
+print('yesterday loss %d %% today profit %0.2f %%' %(a,b)) #### %% repesent '%' symbol
+          
