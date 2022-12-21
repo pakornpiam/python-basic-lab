10248f52daf54273e1bc3a7b969f49dbfdd3317e
pakornpiam <pakornpiam@gmail.com>
2022-12-21 16:50:41 +0700

    done

---
+++ 500 python/print string.py
@@ -0,0 +1,22 @@
+print('hello')
+print('hello world')
+print('hello_python')
+print('/\\/\\/\\')
+print('a''\n' 'an''\n' 'ant''\n')
+
+print('        *      ''\n''*       *      *''\n''        *      ')
+print(''' \t*
+         *\t*\t*
+          \t*''')
+print('................................')
+print('''\t*\t+\t*
+        +\t*\t+
+        *\t+\t*''')
+
+print('Just beacuase something\nthinks differancly from you.\ndoes that mean it s not thinking?')
+
+
+
+print('''\t\\ \t /
+             X
+         \\       /''')
